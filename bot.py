d00061bd70357395991dd45be08483f60fb38474
gabil4ator-afk <gabil4ator@gmail.com>
2026-05-30 18:38:26 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -469,16 +469,22 @@ async def parse_listing(url):
         async with async_playwright() as p:
 
             browser = await p.chromium.launch(
-                headless=True,
-                args=[
-                    "--disable-blink-features=AutomationControlled"
-                ]
-            )
-
-            page = await browser.new_page(
-    user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/128.0.0.0 Safari/537.36"
+    headless=True,
+    args=[
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--disable-blink-features=AutomationControlled"
+    ]
 )
 
+           page = await browser.new_page(
+    viewport={
+        "width": 1280,
+        "height": 720
+    }
+)
             #await stealth(page)
 
             await page.set_extra_http_headers({
@@ -490,10 +496,17 @@ async def parse_listing(url):
                 timeout=30000,
                 wait_until="domcontentloaded"
             )
+        await page.set_user_agent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/131.0.0.0 Safari/537.36"
+)
 
             await page.wait_for_timeout(3000)
 
-            html = await page.content()
+           print("URL:", page.url)
+print("TITLE:", await page.title())
+        html = await page.content()
 
             await browser.close()
 
